2dccda942009b8a57d01b0f7b45f64ebccb655a2
nskwb <nishimaru08@gmail.com>
2021-06-14 14:23:26 +0900

    [update]Header design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,9 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
+import "../css/tailwindcss.css";
+import "stylesheets/application";
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
-
-import "../css/tailwindcss.css";
-import "stylesheets/application";

e8e89bdccdd854c3a10e188f388647fed9907945
nskwb <nishimaru08@gmail.com>
2021-06-19 14:48:54 +0900

    [Update]Display posts in descending order of creation date

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  default_scope -> { order(created_at: :desc) }
+
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
   validates :content, presence: true, length: { maximum: 200 }
   validates :user_id, presence: true
@@ -10,5 +12,6 @@ class Post < ApplicationRecord
   validates :fat, presence: true
   validates :carbo, presence: true
   validates :calorie, presence: true
+
   mount_uploader :image, ImageUploader
 end

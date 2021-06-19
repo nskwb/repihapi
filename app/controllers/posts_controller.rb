a98c19ba1f9ba01fa862371eb93b74fabe9e78f9
nskwb <nishimaru08@gmail.com>
2021-06-19 14:06:28 +0900

    [Add]Edit a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: %i[index create destroy]
-  before_action :correct_user, only: :destroy
+  before_action :authenticate_user!, only: %i[create edit update destroy]
+  before_action :correct_user, only: %i[edit update destroy]
 
   def index
     @posts = Post.page(params[:page]).per(15)
@@ -24,6 +24,20 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = '投稿を更新しました'
+      redirect_to post_path
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @post.destroy
     flash[:success] = '投稿を削除しました'
@@ -37,7 +51,7 @@ class PostsController < ApplicationController
   end
 
   def correct_user
-    @post = current_user.posts.find_by(id: params[:id])
-    redirect_to request.referer if @post.nil?
+    @post = Post.find(params[:id])
+    redirect_to request.referer unless @post.user == current_user
   end
 end

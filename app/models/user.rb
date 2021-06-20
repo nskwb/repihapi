9b4f49bd1d7ad1d6b9bfd820241f7d25fc588b13
nskwb <nishimaru08@gmail.com>
2021-06-20 11:43:16 +0900

    [Update]Dependence on User model-Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_tweets, through: :favorites, source: :post
 
-  has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id, dependent: :destroy
   has_many :follows, through: :active_relationships, source: :follower
 
-  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :following
 
   mount_uploader :image, ImageUploader

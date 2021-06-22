59ec674dad765105888c0ca4f0191675fb0b6cfb
nskwb <nishimaru08@gmail.com>
2021-06-22 17:10:38 +0900

    [Update]Redirect to user page after user update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -48,6 +48,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
+
+  def after_update_path_for(resource)
+    user_path(current_user)
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])

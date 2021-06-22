36577b290dff0cd7016ac972309e3ed1e887377e
nskwb <nishimaru08@gmail.com>
2021-06-22 17:06:08 +0900

    [Update]Retain image even if user don't pass validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email image image_cache])
   end
 end

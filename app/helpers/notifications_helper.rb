0fdb41943c75388108a84d3b908f8148c66b2af2
nskwb <nishimaru08@gmail.com>
2021-06-25 14:41:13 +0900

    [Update]Change list style of notification page to none

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,16 +1,17 @@
 module NotificationsHelper
   def notification_info(notification)
-    @visitor = notification.visitor
-    @user_link = tag.a(@visitor.name, href: user_path(@visitor))
+    visitor = notification.visitor
+    user_link = tag.a(visitor.name, href: user_path(visitor))
     @post_link = tag.a('あなたの投稿', href: post_path(notification.post_id)) if notification.post_id
+    @user_info = user_link + 'さんが'
     case notification.action
     when 'follow'
-      "#{@user_link}さんがあなたをフォローしました"
+      @user_info + 'あなたをフォローしました'
     when 'favorite'
-      "#{@user_link}さんが#{@post_link}をいいねしました"
+      @user_info + @post_link + 'をいいねしました'
     when 'comment'
       @comment = Comment.find_by(id: notification.comment_id)
-      "#{@user_link}さんが#{@post_link}にコメントしました"
+      @user_info + @post_link + 'にコメントしました'
     end
   end
 end

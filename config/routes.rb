3cc46a8cb035fd38ff1c0527eb85f9fcec73709e
nskwb <nishimaru08@gmail.com>
2021-07-03 18:47:36 +0900

    [Add]Routes for meal records

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,43 @@
-Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
-  root 'posts#index'
-  get 'posts/show'
-  get 'relationships/create'
-  get 'relationships/destroy'
-  devise_for :users, controllers: { confirmations: 'users/confirmations',
-                                    registrations: 'users/registrations',
-                                    passwords: 'users/passwords',
-                                    omniauth_callbacks: 'users/omniauth_callbacks' }
+Rails
+  .application
+  .routes
+  .draw do
+    get 'favorites/create'
+    get 'favorites/destroy'
+    root 'posts#index'
+    get 'posts/show'
+    get 'relationships/create'
+    get 'relationships/destroy'
+    devise_for :users,
+               controllers: {
+                 confirmations: 'users/confirmations',
+                 registrations: 'users/registrations',
+                 passwords: 'users/passwords',
+                 omniauth_callbacks: 'users/omniauth_callbacks',
+               }
 
-  devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-  end
-
-  resources :users do
-    resource :relationships, only: %i[create destroy]
-    member do
-      get :follows
-      get :followers
-      get :favorites
+    devise_scope :user do
+      post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     end
 
-    collection do
-      get 'search'
+    resources :users do
+      resource :relationships, only: %i[create destroy]
+      member do
+        get :follows
+        get :followers
+        get :favorites
+        get :meal_records
+      end
+
+      collection { get 'search' }
     end
-  end
 
-  resources :posts do
-    resource :favorites, only: %i[create destroy]
-    resource :comments, only: :create
-
-    collection do
-      get 'search'
+    resources :posts do
+      resource :favorites, only: %i[create destroy]
+      resource :comments, only: :create
+      resource :meal_records, only: %i[create destroy]
+      collection { get 'search' }
     end
-  end
 
-  resources :notifications, only: :index
-end
+    resources :notifications, only: :index
+  end

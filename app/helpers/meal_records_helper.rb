d0f1cbc458d94a937f9c088fdf799fd908e4c1c0
nskwb <nishimaru08@gmail.com>
2021-07-03 19:03:08 +0900

    [Add]MealRecords controller

---
+++ app/helpers/meal_records_helper.rb
@@ -0,0 +1,2 @@
+module MealRecordsHelper
+end
---
+++ test/controllers/meal_records_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MealRecordsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

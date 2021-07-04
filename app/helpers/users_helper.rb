f8c3aa55f9154fa2dc218beb5b9ecc0bd192daa3
nskwb <nishimaru08@gmail.com>
2021-07-04 15:34:26 +0900

    [Update] Front-end | Change the calendar design on the meal record page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,5 @@
 module UsersHelper
-  meal_record = nil
-
   def guest_user?
     current_user.email == 'guest@repihapi.com'
-  end
-
-  def meal_record_present?(arg)
-    meal_record = Post.find_by(id: arg.post_id)
-    meal_record.present?
   end
 end

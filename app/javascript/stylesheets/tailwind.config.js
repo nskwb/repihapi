69e70fd6db6ee410dcc48853b99c06b3ea695a45
nskwb <nishimaru08@gmail.com>
2021-07-16 18:41:45 +0900

    [Update]Front-end | Configure purge css

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ["app/views/**/*.html.erb", "app/javascript/**/*.css"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},

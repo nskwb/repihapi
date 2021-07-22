2abc8f4f3635cbc0c6484bf9c69f3acec257bdd9
nskwb <nishimaru08@gmail.com>
2021-07-22 17:05:12 +0900

    [Fix]typo in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "../css/tailwindcss.css";
-import "stylesheets/application.scss";
+import "/app/assets/stylesheets/application.scss";
 require("jquery");
 import "@nathanvda/cocoon";
 import "@fortawesome/fontawesome-free/js/all";

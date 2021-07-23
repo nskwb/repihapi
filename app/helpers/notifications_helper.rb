49909977f4ed7dd6cbad73fe5b39eb4f4b14871e
nskwb <nishimaru08@gmail.com>
2021-07-23 14:26:49 +0900

    [Update]Front-end | design of notification page

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,8 +1,8 @@
 module NotificationsHelper
   def notification_info(notification)
     visitor = notification.visitor
-    user_link = tag.a(visitor.name, href: user_path(visitor))
-    @post_link = tag.a('あなたの投稿', href: post_path(notification.post_id)) if notification.post_id
+    user_link = tag.a(visitor.name, href: user_path(visitor), class: "text-xl font-bold text-green-600 underline")
+    @post_link = tag.a('あなたの投稿', href: post_path(notification.post_id), class: "text-green-600 font-bold underline") if notification.post_id
     @user_info = user_link + 'さんが'
     case notification.action
     when 'follow'

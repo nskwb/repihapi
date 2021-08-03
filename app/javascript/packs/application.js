bebc49d4efc9dfec0e7b2f29cbbe492dac8a1919
nskwb <nishimaru08@gmail.com>
2021-08-04 01:46:59 +0900

    [Update]Add process for handling images in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,7 @@ require("slick.js");
 import "../css/tailwind.css";
 
 const images = require.context("../images/", true);
+const imagePath = (name) => images(name, true);
 
 Rails.start();
 Turbolinks.start();

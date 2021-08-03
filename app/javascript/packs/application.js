aead39dfca01b2c5dc17fd688b98b193dd810c93
nskwb <nishimaru08@gmail.com>
2021-08-03 23:25:41 +0900

    [Update]Config of packs/application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
 require("jquery");
 require("@nathanvda/cocoon");
 require("slick.js");
 
 import "../css/tailwind.css";
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();

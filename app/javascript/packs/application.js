3318d87c7383e7da1545dc2de2dc77ae4c70f0f1
nskwb <nishimaru08@gmail.com>
2021-08-03 22:00:11 +0900

    [Update]Configure of Tailwindcss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-import "../css/tailwindcss.css";
-import "../stylesheets/application.scss";
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+
 require("jquery");
-import "@nathanvda/cocoon";
-require("./pages/slick");
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
+require("@nathanvda/cocoon");
+require("slick.js");
+
+import "../css/tailwind.css";

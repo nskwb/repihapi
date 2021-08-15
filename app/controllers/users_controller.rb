c7f0b8f7917cc0b58afd469de56599b4cb52e426
nskwb <nishimaru08@gmail.com>
2021-08-15 18:04:21 +0900

    [Update]users_controller#meal_records

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,23 +31,12 @@ class UsersController < ApplicationController
     @meal_records = current_user.meal_records.where(created_at: @start_date.all_month)
 
     if @meal_records.present?
-      # current_user.calculate_nutrients(@meal_records)
-      sum_protein = 0
-      sum_fat = 0
-      sum_carbo = 0
-      sum_calorie = 0
+      count = @meal_records.length
 
-      counts = @meal_records.count
-      @meal_records.each do |meal_record|
-        sum_protein += meal_record.post_protein
-        sum_fat += meal_record.post_fat
-        sum_carbo += meal_record.post_carbo
-        sum_calorie += meal_record.post_calorie
-      end
-      @average_protein = (sum_protein / counts).round
-      @average_fat = (sum_fat / counts).round
-      @average_carbo = (sum_carbo / counts).round
-      @average_calorie = (sum_calorie / counts).round
+      @average_protein = @meal_records.average(:post_protein).round
+      @average_fat = @meal_records.average(:post_fat).round
+      @average_carbo = @meal_records.average(:post_carbo).round
+      @average_calorie = @meal_records.average(:post_calorie).round
 
       @total = @average_protein + @average_fat + @average_carbo
       @ratio_protein = (@average_protein / @total.to_f * 100).round

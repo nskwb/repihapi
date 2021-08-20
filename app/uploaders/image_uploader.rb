de1f065db5558cacaf37c7964a379d19d29638b3
nskwb <nishimaru08@gmail.com>
2021-08-20 17:27:11 +0900

    Revert "[Add]carrierwave configuration file and environment variables#49"
    This reverts commit 551dbab043b2394186916e64b0938921bd1a7de4.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,11 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fit: [300, 300]
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:

4246224ccf87eab42f25d7bad423e93bea793cd4
nskwb <nishimaru08@gmail.com>
2021-08-23 13:25:03 +0900

    [Remove]Function to send email for password resetting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable,
          :registerable,
-         :recoverable,
          :rememberable,
          :validatable,
          :omniauthable,
